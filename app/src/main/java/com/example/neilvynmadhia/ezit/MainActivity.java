ac228d5b5a2ada13fb321db432ef615e8e96996b
Vainsyl <sylvain_rcs@hotmail.com>
2015-12-01 14:03:39 +0100

    Test

--- app/src/main/java/com/example/neilvynmadhia/ezit/MainActivity.java
+++ app/src/main/java/com/example/neilvynmadhia/ezit/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.neilvynmadhia.ezit;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,15 +11,89 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+<<<<<<< Updated upstream
 import com.facebook.FacebookSdk;
 //ga0RGNYHvNM5d0SLGQfpQWAPGJ8=
+=======
+import android.provider.ContactsContract.Contacts;
+
+
+
+>>>>>>> Stashed changes
 public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+<<<<<<< Updated upstream
         FacebookSdk.sdkInitialize(getApplicationContext());
         // Initialize the SDK before executing any other operations,
         // especially, if you're using Facebook UI elements.
+=======
+        setContentView(R.layout.firstpage);
+        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+         FloatingActionButton button = (FloatingActionButton) findViewById(R.id.fab);
+         button.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+         .setAction("Action", null).show();
+            getContact();
+
+        // Comment
+    }
+     });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+>>>>>>> Stashed changes
+    }
+
+
+    public void getContact() {
+        ContentResolver cr = getContentResolver();
+        Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        while (cursor.moveToNext()) {
+            //get name
+            int nameFiledColumnIndex = cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME);
+            String contact = cursor.getString(nameFiledColumnIndex);
+
+            String[] PHONES_PROJECTION = new String[]{"_id", "display_name", "data1", "data3"};//
+            String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup._ID));
+            Cursor phone = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, PHONES_PROJECTION,
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contactId, null, null);
+            //name type ..
+            while (phone.moveToNext()) {
+                int i = phone.getInt(0);
+                String str = phone.getString(1);
+                str = phone.getString(2);
+                str = phone.getString(3);
+            }
+            phone.close();
+
+        }
+        cursor.close();
     }
 }
+
